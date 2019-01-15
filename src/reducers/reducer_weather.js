736bddf1a44e7c3970ea76babb0ae10fc0af0800
1101029(Jihye Choi) <coalcellar@sk.com>
2019-01-15 17:48:10 +0900

    prepare for weather list container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,14 @@
-export default (state = null, action) => {
+import { FETCH_WEATHER } from "../actions/index";
+
+export default (state = [], action) => {
     console.log('Action received ', action);
+
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // 여러 도시를 가지고 올 것이므로 배열에 넣음
+            // return state.concat([action.payload.data])
+            return [action.payload.data, ...state]
+    }
 
     return state
 }
