af906c3aced7fb96d800dd69ac96af4c00c61069
1101029(Jihye Choi) <coalcellar@sk.com>
2019-01-17 18:28:18 +0900

    google map setCenter lat, lon type ... T.T

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,12 +1,15 @@
+import _ from 'lodash'
 import React from 'react'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
 export default withGoogleMap((props) => {
-    console.log(typeof props.lon)
+        // console.log(typeof props.lon)
+        const {lat, lon} = props;
+        // console.log('lat : ', lat, ', lon: ', lon);
         return (
             <GoogleMap
                 defaultZoon={12}
-                defaultCenter={{lat: props.lat, lon: props.lon}}
+                defaultCenter={{lat: parseFloat(lat), lon: parseFloat(lon)}}
             />
         )
     }
